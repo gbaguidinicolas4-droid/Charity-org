34da952c923dd8948b1d98cfa6fd2b92df9dfb88
Nicolas GBAGUIDI <gbaguidinicolas4@gmail.com>
2026-08-12 08:38:30 +0100

    Remove unused login page

--- app/auth/register/page.tsx
+++
@@ -1,8 +0,0 @@
-import Register from "@/features/auth/components/Register";
-export default function RegisterPage() {
-  return (
-    <main className="relative min-h-screen">
-      <Register />
-    </main>
-  );
-}
